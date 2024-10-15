893c569b3f1f497aebeb69dc995a9f69e9c0444d
prager <jkulisek.us@gmail.com>
2024-10-14 17:21:19 -0700

    Added content framework

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        //return view('welcome_message');
         echo view('template/header');
         echo view('public/main_view');
         echo view('template/footer');
